57a0608dc7a1af8d1de4ca00a7018b62361cfa0e
jainayu <500060884@stu.upes.ac.in>
2020-09-29 16:14:08 +0530

    BST complete

--- Trees/BST.cpp
+++ Trees/BST.cpp
@@ -9,21 +9,77 @@ private:
 public:
 	BST();
 	BST(int);
-	insert(int);
-}
+	BST* insert(BST*, int);
+	void inOrder(BST *);
+	void preOrder(BST *);
+	void postOrder(BST *);
+};
 
 BST :: BST() : data(0), left(NULL), right(NULL) {}
 
 BST :: BST(int val) {
 	data = val;
-	left = right = NULL:
+	left = right = NULL;
 }
 
-void BST :: insert(int data) {
+BST* BST :: insert(BST* node, int val) {
+	if(!node) {
+		return new BST(val);
+	}
 	
+	if(val > node->data) {
+		node->right = insert(node->right, val);
+	} else {
+		node->left = insert(node->left, val);
+	}
+	return node;
 }
 
-int mian(){
+void BST :: inOrder(BST *root) { 
+    if(!root) return; 
+    
+	inOrder(root->left); 
+	cout << root->data << " "; 
+	inOrder(root->right);
+}
+
+void BST :: preOrder(BST *root) { 
+    if(!root) return; 
+    
+    cout << root->data << " "; 
+	preOrder(root->left); 
+	preOrder(root->right);
+} 
+ 
+void BST :: postOrder(BST *root) { 
+    if(!root) return; 
+    
+	postOrder(root->left); 
+	postOrder(root->right);
+	cout << root->data << " "; 
+}
+
+int main() {
+	BST bst, *root = NULL; 
+    root = bst.insert(root, 6); 
+    bst.insert(root, 5); 
+    bst.insert(root, 10); 
+    bst.insert(root, 4); 
+    bst.insert(root, 7); 
+    bst.insert(root, 15); 
+    bst.insert(root, 9); 
+    
+    cout << "In-order traversal: ";
+	bst.inOrder(root);
+	cout << endl;
+	
+	cout << "Pre-order traversal: ";
+	bst.preOrder(root);
+	cout << endl;
+
+    cout << "Post-order traversal: ";
+	bst.postOrder(root);
+	cout << endl;
 
 	return 0;
 }
